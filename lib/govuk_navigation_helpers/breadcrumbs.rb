dcf35560de772eb19498ee331f3fd63b0f89113a
Carlos Vilhena <carlosvilhena@gmail.com>
2016-12-14 12:21:11 +0000

    Convert the content item into a ContentItem
    This avoids using hash accessors in this class. The `ContentItem` class
    already has logic to access relevant parts of the content item hash,
    there is no need to duplicate that logic here.

--- lib/govuk_navigation_helpers/breadcrumbs.rb
+++ lib/govuk_navigation_helpers/breadcrumbs.rb
@@ -1,21 +1,21 @@
 module GovukNavigationHelpers
   class Breadcrumbs
     def initialize(content_item)
-      @content_item = content_item
+      @content_item = ContentItem.new(content_item)
     end
 
     def breadcrumbs
-      direct_parent = content_item.dig("links", "parent", 0)
+      direct_parent = content_item.parent
 
       ordered_parents = []
 
       while direct_parent
         ordered_parents << {
-          title: direct_parent["title"],
-          url: direct_parent["base_path"],
+          title: direct_parent.title,
+          url: direct_parent.base_path,
         }
 
-        direct_parent = direct_parent.dig("links", "parent", 0)
+        direct_parent = direct_parent.parent
       end
 
       ordered_parents << { title: "Home", url: "/" }
