de64aa0a71f134247bf7e2303ca99d1da83c0051
David Silva <david.silva@digital.cabinet-office.gov.uk>
2017-12-07 09:31:05 +0000

    wip: adds get-divorce json

--- lib/govuk_navigation_helpers/tasklist_content.rb
+++ lib/govuk_navigation_helpers/tasklist_content.rb
@@ -4,13 +4,19 @@ module GovukNavigationHelpers
       new.parse_file("learn-to-drive-a-car.json")
     end
 
+    def self.get_a_divorce_config
+      new.parse_file("get-a-divorce.json")
+    end
+
     def parse_file(file)
       @file ||=
         JSON.parse(
           File.read(
             File.join(File.dirname(__FILE__), "../../", "config", "tasklists", file)
           )
-        ).deep_symbolize_keys
+        ).deep_symbolize_keys.tap do |json_file|
+          json_file[:tasklist].merge!({small: true, heading_level: 3})
+        end
     end
   end
 end
--- spec/tasklist_content_spec.rb
+++ spec/tasklist_content_spec.rb
@@ -24,5 +24,28 @@ module GovukNavigationHelpers
         expect(first_link[:text]).to eql("Check what age you can drive")
       end
     end
+
+    context "get a divorce tasklist" do
+      before do
+        @config = described_class.get_a_divorce_config
+      end
+
+      it "configures a sidebar" do
+        expect(@config[:tasklist][:heading_level]).to eql(3)
+        expect(@config[:tasklist][:small]).to be true
+      end
+
+      it "has symbolized keys" do
+        @config.keys.each do |key|
+          expect(key.is_a?(Symbol)).to be true
+        end
+      end
+
+      it "has a link in the correct structure" do
+        first_link = @config[:tasklist][:groups][0][0][:contents][1][:links][0]
+        expect(first_link[:href]).to eql("https://www.relate.org.uk/relationship-help/help-separation-and-divorce")
+        expect(first_link[:text]).to eql("Get advice from Relate")
+      end
+    end
   end
 end
